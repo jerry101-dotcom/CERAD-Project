996ab735ffca45ca066b3fc1ee4c221394adab03
jerry101-dotcom <luckyomokefe@gmail.com>
2024-02-26 14:15:49 +0100

    New files

---
+++ school-confrence/home.js
@@ -0,0 +1,72 @@
+// let dropdownList = document.querySelector("#dropdown")
+// let dropMenu = document.querySelector("#drop")
+// let bar = document.querySelector("#bar")
+// let a = document.getElementById("hh")
+    
+
+// bar.addEventListener("mouseover", (e) =>{
+//     e.preventDefault()
+//     dropMenu.innerHTML = `
+//     <div id="ggg" class="ggg">
+//     <li class="list-3"><a class="header-link-2"href="home.html">Home</a></li>
+//     <li class="list-3"><a class="header-link-2"href="who-we-are.html">About us</a></li>
+//     <li class="list-3"><a class="header-link-2"href="contact.html">Contact</a></li>
+//     <li class="list-3"><a class="header-link-2"href="academic.html">Academic Events</a></li>
+//     <li class="list-3"><a class="header-link-2"href="publication.html">Publication</a></li>
+//     <li class="list-3"><a class="header-link-2"href="randd.html">R & D Forum</a></li>
+//     <li class="list-3"><a class="header-link-2"href="submit.html">Submit Abstract</a></li>
+//     <li class="list-3"><a class="header-link-2"href="submit-article.html">Paper Submission</a></li>
+//     <li class="list-3"><a class="header-link-2"href="adhoc.html">ADHOC Members</a></li>
+//     </div>
+//     `
+    
+//     dropMenu.style.opacity= "1"
+//     dropMenu.style.visibility = "visible"
+//     dropMenu.style.transition = ".5s"
+//     console.log("clicked")
+// })
+
+
+// window.addEventListener("click", (e)=>{
+//     e.preventDefault()
+//     dropMenu.style.opacity= "0"
+//     dropMenu.style.visibility = "hidden"
+//     dropMenu.style.transition = ".5s"
+// })
+
+
+
+// dropdownList.addEventListener("mouseout", (e)=>{
+//     e.preventDefault()
+//     dropMenu.style.opacity= "0"
+//     dropMenu.style.visibility = "hidden"
+// })
+
+// dropMenu.addEventListener("mouseover", (e) =>{
+//     e.preventDefault()
+//     dropMenu.innerHTML = `
+//     <li class="list-3"><a class="header-link-2"href="academic.html">Academic Events</a></li>
+//     <li class="list-3"><a class="header-link-2"href="publication.html">Publication</a></li>
+//     <li class="list-3"><a class="header-link-2"href="randd.html">R & D Forum</a></li>
+//     <li class="list-3"><a class="header-link-2"href="submit.html">Submit Abstract</a></li>
+//     <li class="list-3"><a class="header-link-2"href="submit-article.html">Paper Submission</a></li>
+//     <li class="list-3"><a class="header-link-2"href="adhoc.html">ADHOC Members</a></li>
+
+//     `
+//     dropMenu.style.opacity= "1"
+//     dropMenu.style.visibility = "visible"
+//     dropMenu.style.transition = ".5s"
+//     console.log(e)
+// })
+
+// dropMenu.addEventListener("mouseout", (e) =>{
+//     e.preventDefault()
+//     dropMenu.style.opacity= "0"
+//     dropMenu.style.visibility = "hidden"
+//     dropMenu.style.transition = ".5s"
+//     console.log(e)
+// })
+
+
+// console.log(window.innerWidth)
+
